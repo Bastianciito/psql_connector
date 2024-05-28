03ac99d55200fab82189bee2612489a19cae3303
Bastiancito <bastian.gamboa@ug.uchile.cl>
2024-05-28 12:51:17 -0400

    update

--- psql_connector/pg_connector.py
+++ psql_connector/pg_connector.py
@@ -5,30 +5,65 @@ from psycopg2.extras import execute_values
 from psycopg2.extensions import register_adapter, AsIs
 import sys
 import numpy as np
+import pandas as pd
+import requests
+import sqlalchemy as sa
 
 
 class PgConnector:
 
-    def addapt_numpy_float64(numpy_float64):
-        return AsIs(numpy_float64)
+    # def addapt_numpy_float64(numpy_float64):
+    #     return AsIs(numpy_float64)
 
-    def addapt_numpy_int64(numpy_int64):
-        return AsIs(numpy_int64)
+    # def addapt_numpy_int64(numpy_int64):
+    #     return AsIs(numpy_int64)
 
-    register_adapter(np.float64, addapt_numpy_float64)
-    register_adapter(np.int64, addapt_numpy_int64)
+    # register_adapter(np.float64, addapt_numpy_float64)
+    # register_adapter(np.int64, addapt_numpy_int64)
 
-    def __init__(self) -> None:
+    def __init__(self, credentials: dict = None, **kwargs) -> None:
         self.conn = None
+        self.engine = None
+        self.credentials = credentials
+
+    def help(self):
+        print(
+            """To create a conection first init the object with the following params .... """
+        )
 
     def create_conn(self, **kwargs):
-        try:
-            self.conn = psycopg2.connect(**kwargs)
-            self.conn.autocommit = True
-            logging.info("connected to postgresql !")
-        except (Exception, psycopg2.DatabaseError) as error:
-            logging.error(error)
-            logging.error("It was not possible to connect to Database")
+
+        if self.credentials is not None:
+            try:
+                self.conn = psycopg2.connect(**self.credentials)
+                self.conn.autocommit = True
+                logging.info("connected to postgresql !")
+            except (Exception, psycopg2.DatabaseError) as error:
+                logging.error(error)
+                logging.error("It was not possible to connect to Database")
+
+        else:
+            logging.warning(
+                "Credentials was not provider, set credentials atribute of PgConnector"
+            )
+
+    def create_sqlalchemy_engine(self, **kwargs):
+
+        if self.credentials is not None:
+            try:
+                engine = sa.create_engine(
+                    f"postgresql+psycopg2://{self.credentials['user']}:{self.credentials['password']}@{self.credentials['host']}/{self.credentials['dbname']}"
+                )
+                engine.begin()
+                self.engine = engine
+            except:
+                logging.error(
+                    "Credential params does not meet conditions, please make sure that the following values are available"
+                )
+        else:
+            logging.warning(
+                "Credentials was not provider, set credentials atribute of PgConnector"
+            )
 
     def create_curs(self):
         return self.conn.cursor()
@@ -37,12 +72,16 @@ class PgConnector:
         self.conn.close()
 
     def sqlio_query(self, query="", **kwargs):
-        try:
-            df_ = sqlio.read_sql_query(query, self.conn)
-            return df_
-        except:
-            logging.error("Something goes wrong")
-            sys.exit()
+
+        if self.engine is not None:
+            try:
+                df_ = sqlio.read_sql_query(query, self.engine)
+                return df_
+            except:
+                logging.error("Something goes wrong")
+                sys.exit()
+        else:
+            logging.warning("SQL engine was not initialized")
 
     def execute_query_fetch(self, query="", **kwargs):
         try:
